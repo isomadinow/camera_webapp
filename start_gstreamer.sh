15ebabf6ed18139b0caf0e38a020950a883aabb1
isomadinovakh <isomadinow@gmail.com>
2025-02-11 23:33:59 +0600

    обновление скрипта sh

--- start_gstreamer.sh
+++ start_gstreamer.sh
@@ -3,7 +3,7 @@
 start_stream() {
     local PORT=$1
     gst-launch-1.0 -v videotestsrc ! videoconvert ! x264enc tune=zerolatency bitrate=500 speed-preset=superfast ! \
-        rtph264pay config-interval=1 pt=96 ! udpsink host=127.0.0.1 port=$PORT &
+        rtph264pay config-interval=1 pt=96 ! udpsink host=172.20.76.82 port=$PORT &
 }
 
 
