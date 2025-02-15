214d0916c1ccade87d93d1c10742189fd6a7eafe
isomadinovakh <isomadinow@gmail.com>
2025-02-15 18:36:15 +0600

    Мелькие доработки

--- run.py
+++ run.py
@@ -1,4 +1,3 @@
-# run.py
 import sys
 import os
 
@@ -9,9 +8,7 @@ sys.path.insert(0, opencv_path)
 import cv2
 from app import create_app
 
-app = create_app()  # Создаём Flask-приложение
+app = create_app() 
 
 if __name__ == '__main__':
-    print(cv2.getBuildInformation())
-    # Запуск на 0.0.0.0:5000
     app.run(host='0.0.0.0', port=5000, debug=True)
